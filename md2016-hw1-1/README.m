87dd22e05efedf53086fd1d85d4642d796366ce8
Han Hao <b01902078@ntu.edu.tw>
2016-09-27 16:09:39 +0800

    Update README.m

--- md2016-hw1-1/README.m
+++ md2016-hw1-1/README.m
@@ -1,22 +1,22 @@
-# instruction to execute the code
+#instruction to execute the code
 
 - full instruction:
 
 ```
+  
 > python hw1-1.py --bigram_file [default=./bigram.txt] --encode_file [default=./encode.txt]  --test_file [default=./test.txt] --output_file [default=./pred.txt]
-
 ```
 
 - brief instruction: (this will use default path directly)
 
 ```
+  
 > python hw1-1.py 
-
 ```
 or
 
 ```
+  
 > chmod a+x ./run.sh
 > ./run.sh
-
 ```
