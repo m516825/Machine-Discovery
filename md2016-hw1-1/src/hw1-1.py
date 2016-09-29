fb5c3d952145ed907c6ab4122a56988536ddd829
m516825 <b01902078@ntu.edu.tw>
2016-09-29 21:36:16 +0800

    prob loading bug fixed

--- md2016-hw1-1/src/hw1-1.py
+++ md2016-hw1-1/src/hw1-1.py
@@ -20,24 +20,16 @@ def load_prob(bigram_file, encode_file, c2i):
 	size = len(c2i)
 	bigram_table = np.zeros((size, size))
 	encode_table = np.zeros((size, size))
+	
 
 	with open(bigram_file, 'r') as f:
 		for line in f.readlines():
-			info = line.strip().split(' ')
-			if len(info) == 3:
-				bigram_table[c2i[info[0]], c2i[info[1]]] = float(info[2])
-			if len(info) == 2:
-				if line.startswith(' '):
-					bigram_table[c2i[' '], c2i[info[0]]] = float(info[1])
-				else:
-					bigram_table[c2i[info[0]], c2i[' ']] = float(info[1])
+			bigram_table[c2i[line[0]], c2i[line[2]]] = float(line.strip().split(' ')[-1])
 
 	# P(z|x) = P(x|z)P(z)/P(x) and P(x) P(x) are same
 	with open(encode_file, 'r') as f:
 		for line in f.readlines():
-			info = line.strip().split(' ')
-			if len(info) == 3:
-				encode_table[c2i[info[0]], c2i[info[1]]] = float(info[2])
+			encode_table[c2i[line[0]], c2i[line[2]]] = float(line.strip().split(' ')[-1])
 	encode_table[c2i[' '], c2i[' ']] = 1.
 
 	return bigram_table, encode_table
@@ -69,34 +61,26 @@ def load_test(test_file):
 
 	return tokens
 
-def count_prob(path, b_table, e_mat, p_mat, l_flag):
+def count_prob(path, b_table, e_mat, p_mat):
 	prob = 0
 	for i, pair in enumerate(path):
 		if i == 0:
 			continue
 		else:
 			pair_pre = path[i-1]
-			if l_flag == 0:
-				if b_table[pair_pre[1], pair[1]] == 0. or e_mat[pair[0], pair[1]] == 0.:
-					prob = -float('Inf')
-					break
-				prob = p_mat[pair_pre[0], pair_pre[1]] + math.log(b_table[pair_pre[1], pair[1]]) + math.log(e_mat[pair[0], pair[1]])
-			else:
-				if b_table[pair_pre[1], pair[1]] == 0.:
-					prob = -float('Inf')
-					break
-				prob = p_mat[pair_pre[0], pair_pre[1]] + math.log(b_table[pair_pre[1], pair[1]])
-
+			if b_table[pair_pre[1], pair[1]] == 0. or e_mat[pair[0], pair[1]] == 0.:
+				prob = -float('Inf')
+				break
+			prob = p_mat[pair_pre[0], pair_pre[1]] + np.log(b_table[pair_pre[1], pair[1]]) + np.log(e_mat[pair[0], pair[1]])
 
 	return prob
 
-def max_path(token ,b_table, e_table, c2i, i2c):
+def viterbi(token ,b_table, e_table, c2i, i2c):
 	
 	size = len(c2i)
 	max_path = []
 	e_mat = []
 	p_mat = []
-	l_flag = 0
 	
 	token += ' '
 
@@ -104,35 +88,45 @@ def max_path(token ,b_table, e_table, c2i, i2c):
 		index = c2i[c]
 		e_mat.append(e_table[:, index])
 		p_mat.append(np.zeros(size))
+		tmp = []
+		for i in range(size):
+			tmp.append([])
+		max_path.append(tmp)
 	e_mat = np.array(e_mat)
 	p_mat = np.array(p_mat)
 
 	for p in range(len(token)):
-		if p == len(token)-1:
-			l_flag = 1
 		if p == 0:
 			for i in range(size):
-				p_mat[0, i] = np.log(e_mat[0, i] * b_table[c2i[' '], i]) if e_mat[0, i] * b_table[c2i[' '], i] != 0. else -float('Inf')
+				p_mat[0, i] = np.log(e_mat[0, i]) + np.log(b_table[c2i[' '], i]) if e_mat[0, i] and b_table[c2i[' '], i] != 0. else -float('Inf')
 		else:
-			max_pair = [(p-1, 0), (p, 0)]
-			max_prob = count_prob(max_pair, b_table, e_mat, p_mat, l_flag)
-			for pre in range(size):
-				for cur in range(size):
+			for cur in range(size):
+				max_pair = [(p-1, 0), (p, cur)]
+				max_prob = count_prob(max_pair, b_table, e_mat, p_mat)
+				for pre in range(size):
 					pair = [(p-1, pre), (p, cur)]
-					tmp_prob = count_prob(pair, b_table, e_mat, p_mat, l_flag)
+					tmp_prob = count_prob(pair, b_table, e_mat, p_mat)
 					if tmp_prob > max_prob:
 						max_prob = tmp_prob
 						max_pair = pair
-			max_path.append(max_pair[0])
+				max_path[p][cur] += max_path[p-1][max_pair[0][1]]
+				max_path[p][cur].append(max_pair[0])
 
-			for cur in range(size):
 				if b_table[max_pair[0][1], cur] == 0. or e_mat[p, cur] == 0.:
 					p_mat[p, cur] = -float('Inf')
 					continue
-				p_mat[p, cur] = p_mat[p, cur] + math.log(b_table[max_pair[0][1], cur]) + math.log(e_mat[p, cur])
+				p_mat[p, cur] = p_mat[p-1, max_pair[0][1]] + math.log(b_table[max_pair[0][1], cur]) + math.log(e_mat[p, cur])
 
+		Mpath = []
+		MaxProb = -float('Inf')
+
+		for i in range(size):
+			if p_mat[len(token)-1, i] > MaxProb:
+				MaxProb = p_mat[len(token)-1, i]
+				Mpath = max_path[len(token)-1][i]
+				
 	ans_str = ''
-	for path in max_path:
+	for path in Mpath:
 		ans_str += i2c[path[1]]
 
 	return ans_str
@@ -152,7 +146,7 @@ def main():
 		ans_list = []
 		print >> sys.stderr, 'total tokens: '+str(len(tokens))
 		for i, token in enumerate(tokens):
-			ans_str = max_path(token, b_table, e_table, c2i, i2c)
+			ans_str = viterbi(token, b_table, e_table, c2i, i2c)
 			ans_list.append(ans_str)
 			print >> sys.stderr, '\rdone processing '+str(i)+str('/')+str(len(tokens))+' tokens',
 		print >> sys.stderr, ''
